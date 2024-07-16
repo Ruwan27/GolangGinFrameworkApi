c1a557e757a19b2c4f7e0c87a2b07b22519c4111
ruwan@soka.ac.jp <ruwan@ruwansokaacjpnoMacBook-Pro.local>
2024-07-16 11:35:59 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+) 
+
+func main()  {
+	router:=gin.New()
+	router.GET("/getData",getData)
+	router.POST("/getPostData",getPostData)
+	router.GET("/getQueryString",getQueryString)
+	router.GET("/getUrlData/:name/:age",getUrlData)
+
+	router.Run()
+
+	
+}
+// hrrp://localhost:8080/getUrlData?name=Mark&age=30
+func getUrlData(ctx *gin.Context) {
+	name:=ctx.Param(("name"))
+	age:=ctx.Param("age")
+	ctx.JSON(200,gin.H{
+		"data":"Hi I am gin framework",
+		"name":name,
+		"age":age,
+	})
+	
+ }
+ //Gin framework api get, post, url param and query string
+// hrrp://localhost:8080/getQueryString?name=Mark&age=30
+func getQueryString(ctx *gin.Context) {
+	name:=ctx.Query(("name"))
+	age:=ctx.Query("age")
+	ctx.JSON(200,gin.H{
+		"data":"Hi I am gin framework",
+		"name":name,
+		"age":age,
+	})
+	
+ }
+func getData(ctx *gin.Context) {
+	ctx.JSON(200,gin.H{
+		"data":"Hi I am gin framework", 
+	})
+	
+ }
+ func getPostData(ctx *gin.Context) {
+	body:=ctx.Request.Body
+	value,_:=ioutil.ReadAll(body)
+	ctx.JSON(200,gin.H{
+		"data":"I am post method",
+		"bodyData":string(value),
+	})
+ }
